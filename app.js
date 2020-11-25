c440a91427d9454fd6add02643c60063be49dec7
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2020-11-25 14:56:37 -0300

    Finished

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
 const app = express();
+const urlencodeParser = bodyParser.urlencoded({extended: false});
 
+
+//Criando conexão
+const sql = mysql.createConnection({
+  
+  host:'localhost',
+  user:'raphapac',
+  password: 'Pac2020$',
+  port: 3306
+});
+sql.query("use nodejs");
 
 //Template engine
 
@@ -11,30 +22,61 @@ app.engine("handlebars", handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
+app.use('/img', express.static('img'));
 
 
+//Routes and tamplates
 
-
-
-//Routes and Templates
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   
-   //res.send('Mensagem');
-  //res.sendFile(__dirname + "/index.html");
-  res.render('index');
-});
-app.get("/javascript", (req, res)=>{
-  
-  res.sendFile(__dirname + '/js/javascript.js');
-});
-app.get("/style", (req, res)=>{
-  
-  res.sendFile(__dirname + '/css/style.css');
+  res.render('index', {id: req.params.id}); 
 });
 
+app.get("/inserir",function(req,res){res.render("inserir");});
+
+app.get("/select/:id?",function(req,res){
+  
+  if(!req.params.id){
+    
+      sql.query("select * from user order by id asc",function(err,results,fields){
+        
+        res.render('select',{data:results});
+      });
+  }
+  
+  else{
+      sql.query("select * from user where id=? order by id asc",[req.params.id],function(err,results,fields){
+          res.render('select',{data:results});
+      });
+  }
+});
+
+
+app.post("/controllerForm",urlencodeParser,function(req,res){
+    sql.query("insert into user values (?,?,?)",[req.body.id,req.body.name,req.body.age]);
+    res.render('controllerForm',{name:req.body.name});
+});
+
+
+app.get('/deletar/:id', (req, res) =>{
+  
+  sql.query("delete from user where id=?", [req.params.id]);
+  res.render('deletar');
+});
+
+app.get("/update/:id",function(req,res){
+  
+  sql.query("select * from user where id=?",[req.params.id],function(err,results,fields){
+      
+      res.render('update',{id:req.params.id,name:results[0].name,age:results[0].age});
+  });
+});
+
+app.post("/controllerUpdate",urlencodeParser,function(req,res){
+  
+  sql.query("update user set name=?,age=? where id=?",[req.body.name,req.body.age,req.body.id]);
+  res.render('controllerUpdate');
+});
 
 //Starter Server
-app.listen(3000, (req, res) =>{
-  
-  console.log('Servidor rodando!');
-});
+app.listen(3000, (req, res) =>{console.log('Servidor rodando!');})
